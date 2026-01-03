b5b57672a993660473260e0a3f8c5838a50c6dcd
keerthisai A S <keerthisaias@gmail.com>
2026-01-03 23:31:55 +0530

    Add files via upload
    A beginner-friendly Python program that performs basic arithmetic operations using user input.

---
+++ simple_calculator.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter the num1="))
+num2=int(input("Enter the num2="))
+print("\n----------SIMPLE CALCULATOR-------------")
+print("Addition=      ",num1+num2)
+print("Subtraction=   ",num1-num2)
+print("Multiplication=",num1*num2)
+print("Division=      ",num1/num2)
